9085fb390f1d1662cc7355e7f26cd1a90e7aa0ba
trivedi089 <satyachbs007@gmail.com>
2026-02-14 14:55:59 +0530

    serializer-method-based-views-decorator

--- api/views.py
+++ api/views.py
@@ -1,9 +1,22 @@
 from django.http import JsonResponse
-from students.models import Student
+from rest_framework import status
 
-# Create your views here.
+from students.models import Student
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+#decorator
+@api_view(['GET'])
 def studentsView(request):
-    data = list(Student.objects.all().values())
-    print(data)
-    return JsonResponse(data, safe=False)
+    # data = list(Student.objects.all().values())
+    # print(data)
+    # return JsonResponse(data, safe=False)
+    #Mthod based views
+    if request.method == 'GET':
+        #Get all the data from the student table
+        students = Student.objects.all()
+        serializer = StudentSerializer(students, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
