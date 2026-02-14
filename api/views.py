c93249d1388289b8659b7a89f928288f086f5e7a
trivedi089 <satyachbs007@gmail.com>
2026-02-14 15:21:29 +0530

    PUT-api

--- api/views.py
+++ api/views.py
@@ -28,7 +28,7 @@ def studentsView(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['GET'])
+@api_view(['GET','PUT'])
 def studentDetailView(request, id):
     try:
         student = Student.objects.get(pk=id)
@@ -38,3 +38,11 @@ def studentDetailView(request, id):
     if request.method == 'GET':
         serializer = StudentSerializer(student)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == 'PUT':
+        serializer = StudentSerializer(student, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
