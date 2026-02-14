cc77bc04a520cf105a0f08c9f3de3dcc5d77ac87
trivedi089 <satyachbs007@gmail.com>
2026-02-14 13:51:23 +0530

    api-app

--- django_rest_main/urls.py
+++ django_rest_main/urls.py
@@ -19,5 +19,9 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('students/', include('students.urls'))
+    # Web Application Endpoints
+    path('students/', include('students.urls')),
+
+    #API Endpoints
+    path('api/v1/',include('api.urls')),
 ]
